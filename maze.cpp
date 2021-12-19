b2ac95594089b704520f9c4d6dc42a3f266685dc
lukacslacko <lukacslacko@gmail.com>
2021-12-19 18:53:02 +0100

    Create rooms

--- maze.cpp
+++ maze.cpp
@@ -1,6 +1,11 @@
 #include "maze.hpp"
 #include <cstdlib>
 
+Room::Room(int w_, int x_, int z_, int y_) {
+    roomtype = ROOM;
+    for (auto wall : walls) wall = WALL;
+}
+
 Maze generete(int w,int x, int z ,int y) {
     Maze maze;
     std::vector<int> szamok;
@@ -8,7 +13,7 @@ Maze generete(int w,int x, int z ,int y) {
         for (int j=0;j<y;j++){
             for (int k=0;k<w;k++){
                 for (int l=0;l<z;l++) {
-                    
+                    maze[Coord{i,j,k,l}] = Room(k, i, l, j);
                 }
             }
         }
--- maze.hpp
+++ maze.hpp
@@ -1,6 +1,7 @@
 #ifndef MAZE_HPP
 #define MAZE_HPP
 
+#include <map>
 #include <vector>
 
 enum Wall {
@@ -14,12 +15,15 @@ enum RoomType {
 };
 
 struct Room {
+    Room(int w_, int x_, int z_, int y_) : x(x_), y(y_), z(z_), w(w_);
     Wall walls[8];
     int w,x, z ,y;
     RoomType roomtype;
 };
 
-typedef std::vector<Room> Maze;
+typedef std::vector<int> Coord;
+
+typedef std::map<Coord, Room> Maze;
 
 Maze generete(int w,int x, int z ,int y);
 
