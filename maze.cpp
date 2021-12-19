ae2f0213bb3d2a01be0eefd630f206e9f0465529
t <t@t.t>
2021-12-19 18:48:44 +0100

    t

--- maze.cpp
+++ maze.cpp
@@ -1,1 +1,17 @@
 #include "maze.hpp"
+#include <cstdlib>
+
+Maze generete(int w,int x, int z ,int y) {
+    Maze maze;
+    std::vector<int> szamok;
+    for (int i=0;i<x;i++){
+        for (int j=0;j<y;j++){
+            for (int k=0;k<w;k++){
+                for (int l=0;l<z;l++) {
+                    
+                }
+            }
+        }
+    }
+    return maze;
+}
